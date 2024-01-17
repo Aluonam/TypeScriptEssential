b0f2063017ea9aa22d96e7870a83fe6c4b16689d
Paula Cano <paulacanoroman@gmail.com>
2024-01-17 18:59:43 +0100

    feat: data type in TS

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ mensaje = "Hola ts";
 console.log(mensaje)
 
 const prueba:number = 10;
+
+
+// Definir tipo de inferencia que es una variable:
+// No siempre es necesario especificar el tipo de dato.
+
+let mensaje2 = "prueba";
+const  numero = 100;
+
+interface Persona{
+    nombre: string
+}
+const arregloPersonas: Array<Persona> = [{nombre: 'Pablo'}, {nombre: 'Pedro'}];
