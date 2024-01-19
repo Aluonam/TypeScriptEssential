ae3ecd7ea14ea9ffc24f25437c284e7998b4745f
Paula Cano <paulacanoroman@gmail.com>
2024-01-19 21:46:37 +0100

    feat: example union type with object

--- dist/unionTypes.ts
+++ dist/unionTypes.ts
@@ -1,6 +1,20 @@
 //UNION TYPES
-// permite la unión de varios tipos de datos
+// permite la unión de varios tipos de datos siginifica OR
 
-let age: number | string = 25;
+let age: number | string = 25; // admite número o string
 age = 30;
 age = 'veinticinco';
+
+// También se pueden unir tipos de objetos
+
+type Punto = {
+    x: number;
+    y: number;
+}
+
+type Localizacion = {
+    lat: number;
+    long: number;
+}
+
+let coordenadas : Punto | Localizacion;
