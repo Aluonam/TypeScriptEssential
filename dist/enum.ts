bbd444347fe89e9229a7eb7f1a57c8abdb2b3d9c
Paula Cano <paulacanoroman@gmail.com>
2024-01-20 20:12:58 +0100

    feat: example use enums

--- dist/enum.ts
+++ dist/enum.ts
@@ -43,3 +43,13 @@ function isDelivered(status: OrderStatus){
     return status === OrderStatus.DELIVERED
 }
 isDelivered(OrderStatus.RETURNED)
+
+const order = {
+    orderNumber: 111111,
+    status: OrderStatus.DELIVERED // Devolverá 2 porque está en la posición 2
+}
+
+const order2 = {
+    orderNumber: 111111,
+    status: OrderStatus.PENDING // Devolverá 0 porque está en la posición 0
+}
