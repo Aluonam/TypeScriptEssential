90c8666e65a59000400f70f48e4e9662b4286249
Paula Cano <paulacanoroman@gmail.com>
2024-02-12 23:34:03 +0100

    feat: add ts

--- dist/exampleAxios.ts
+++ dist/exampleAxios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-axios.get('https://jsonplaceholder.typicode.com/users/1').then((response)=>{
-    console.log('yeahhh!')
+
+axios.get<boolean>('https://jsonplaceholder.typicode.com/users/1').then((response)=>{
+    console.log('yeahhh!');
+    const {data} = response;
+    data
 }).catch((error)=>{
     console.log('error', error)
 })
